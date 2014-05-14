8fe06d293a50daefbbfe9018726c7e0b1812e00c
rnentjes <rien@nentjes.com>
2014-05-14 22:46:16 +0200

    sprites

--- opengl-util/sprites.nim
+++ opengl-util/sprites.nim
@@ -1,4 +1,5 @@
 import opengl
+import shaderProgram
 import mesh
 
 type
@@ -6,12 +7,45 @@ type
     x: float32
     y: float32
     texture: GLuint
+    dead: bool
+
+  PSprite* = ref TSprite
 
   TSpriteBatch = object
-    sprites: seq[TSprite]
+    sprites: seq[PSprite]
     count: GLsizei
+    mesh: PMesh
+    shader: PShaderProgram
 
-proc createSpriteBatch* =
-  discard
+  PSpriteBatch* = ref TSpriteBatch
 
+proc createSpriteBatch*() : PSpriteBatch =
+  result = new(TSpriteBatch)
+
+  result.sprites = newSeq[PSprite](0)
+
+  add(result.sprites, new(TSprite))
+  add(result.sprites, new(TSprite))
+
+proc createSprite*() : PSprite = 
+  result = new(TSprite)
+
+proc Delete*(sprite: PSprite) =
+  sprite.dead = true
+
+proc Add*(batch: PSpriteBatch, sprite: PSprite) =
+  add(batch.sprites, sprite)
+
+proc Cleanup*(batch: PSpriteBatch) =
+  var i = 0
+  while i < len(batch.sprites):
+    var sprite = batch.sprites[i]
+
+    if sprite.dead:
+      var replace = pop(batch.sprites)
+      batch.sprites[i] = replace
+
+
+
+  
   
